ea88cf965fcceb6567819d7e00a13c3b62ea3c9a
Andrew A <44456213+t-rhex@users.noreply.github.com>
2021-03-06 11:20:13 -0600

    Create naturalnumber.cpp

---
+++ naturalnumber.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+   
+  int i;
+  
+  cout << " Find the first 10 natural numbers:\n";
+  cout << " The first 10 natural numbers are :\n";
+  
+  for ( i = 1 ; i <= 10 : i++ ) {
+    
+    cout << " The 10 natural numbers are " << i << "" << endl;
+    
+    
+}
