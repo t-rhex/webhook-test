47cde78021e7b842b8e15d2159a139472591426c
Andrew A <44456213+t-rhex@users.noreply.github.com>
2021-03-06 13:13:10 -0600

    Update naturalnumber.cpp
    change

--- naturalnumber.cpp
+++ naturalnumber.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main (){
    
-  int i,n;
+  int i,n , sum=0;
   
   
   cout << " Enter the number you want to find the natural number of: " << endl;
@@ -15,7 +15,7 @@ int main (){
    for ( i = 1 ; i <= n ; i++ ) {
     
     cout << " The 10 natural numbers are " << i << "" << endl;
-    int sum = sum + i;  
+    sum = sum + i;  
   }
     
       cout << " The sum of the " << n << "number is " << sum << endl;
